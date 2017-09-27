9e204eb63891b3ff79acda8cbaf7192e6d9d3a1a
Arturo Reyes <reyesm93@gmail.com>
2017-09-27 18:47:35 -0500

    Set View Controllers

--- Virtual Tourist/AlbumVC.swift
+++ Virtual Tourist/AlbumVC.swift
@@ -1,25 +1,26 @@
 //
-//  ViewController.swift
+//  AlbumVC.swift
 //  Virtual Tourist
 //
 //  Created by Arturo Reyes on 9/27/17.
 //  Copyright © 2017 Arturo Reyes. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class AlbumVC: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
---
+++ Virtual Tourist/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Arturo Reyes on 9/27/17.
+//  Copyright © 2017 Arturo Reyes. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
