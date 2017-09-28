aade1e5fb343c60f674a62ff2f37cb2b6e2616db
Arturo Reyes <reyesm93@gmail.com>
2017-09-27 20:22:20 -0500

    Set map view and addpin functions

--- Virtual Tourist/MapVC.swift
+++ Virtual Tourist/MapVC.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import MapKit
 
-class MapVC: UIViewController {
-
+class MapVC: UIViewController, MKMapViewDelegate {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(self.addPin(_:)))
+        longPress.minimumPressDuration = 1.5
+        mapView.addGestureRecognizer(longPress)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @objc func addPin(_ recognizer: UIGestureRecognizer) {
+        
+        let pressedAt = recognizer.location(in: self.mapView)
+        let pressedAtCoordinate: CLLocationCoordinate2D = mapView.convert(pressedAt, toCoordinateFrom: mapView)
+        
+        let newPin = MKPointAnnotation()
+        newPin.coordinate = pressedAtCoordinate
+        mapView.addAnnotation(newPin)
+        
     }
-
-
+}
+    
+extension MKMapViewDelegate {
+        
+     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
 }
 
