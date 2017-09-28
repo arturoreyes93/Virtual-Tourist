aa9c72e85aee3102010206134bd13a3ce87265f7
Arturo Reyes <reyesm93@gmail.com>
2017-09-27 21:46:48 -0500

    Create FlickerClient

---
+++ Virtual Tourist/FlickrClient.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrClient.swift
+//  Virtual Tourist
+//
+//  Created by Arturo Reyes on 9/27/17.
+//  Copyright © 2017 Arturo Reyes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FlickrClient: NSObject {
+    
+    
+    
+    
+}
